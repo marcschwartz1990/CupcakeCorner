eb4545f87e458d197b921b0f0379cd55d3ba2137
Marc Schwartz <marcschwartz1990@gmail.com>
2022-09-14 10:10:18 -0400

    first commit

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,26 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Marc Schwartz on 9/13/22.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if !specialRequestEnabled {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Cupcake Corner: Project 10 of 100 Days of SwiftUI
